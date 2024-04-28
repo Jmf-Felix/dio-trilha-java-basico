0e6b7bcb2e0bec135edf4b93adada13514b7284d
João Marcos Felix <felixjoaomarcos0@gmail.com>
2024-04-28 19:22:57 -0300

    teste-basico-conta-banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
         
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor digite o numero da sua conta: ");
+        int numero = scanner.nextInt();
+        System.out.println("Por favor digite o numero da sua Agencia: ");
+        String agencia = scanner.next();
+        System.out.println("Por favor digite seu Nome completo: ");
+        scanner.nextLine();
+        String nomeCliente = scanner.nextLine();
+        System.out.println("Por favor digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        
+
+        System.out.println("Olá "+nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo de:" + saldo + " já está disponível para saque.");
     }
     
 }
